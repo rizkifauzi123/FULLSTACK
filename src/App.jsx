7a5e588215b38af97bb32bfeea630689f74ee153
rizkifauzi123 <rizkiahmadfauzi1215@gmail.com>
2025-04-22 18:15:05 +0700

    tugas2 react-js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-// src/App.jsx
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      <Team />
-      <Contact />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/team">Team</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </Router>
   );
 };
 
 export default App;
-
-
